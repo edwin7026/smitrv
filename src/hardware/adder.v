3aed0cf0c8c870d0b425a87bc36d242ed459c8c9
edwin7026 <edwin.joy@incoresemi.com>
2023-04-02 17:18:41 +0530

    Clock based verilog add program

--- src/hardware/adder.v
+++ src/hardware/adder.v
@@ -1,15 +1,20 @@
 // This is a simple example of a 4-bit adder
 
-module adder (A,
-            B,
-            Sum
+module adder (CLK,
+            input1,
+            input2,
+            sum
     );
 
-    input [3:0] A;
-    input [3:0] B;
+    input CLK;
+    input [3:0] input1;
+    input [3:0] input2;
 
-    output [3:0] Sum;
-
-    assign Sum = A + B;
+    output reg [3:0] sum;
+    
+    // A clock activated updation to demonstrate framework features
+    always @(posedge CLK) begin
+        sum = input1 + input2;
+    end
         
 endmodule
