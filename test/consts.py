e9f0c7a6a714de433b898cb93d4b3016fb5e3be2
edwin7026 <edwin.joy@incoresemi.com>
2023-04-02 17:19:45 +0530

    Comments

--- test/consts.py
+++ test/consts.py
@@ -12,7 +12,6 @@ model_path = root_path + '/src/models/'
 test_root = root_path + '/test/'
 test_file_dir = test_root + '/test_bench/'
 sim_build = test_root + '/sim_build/'
-work_dir = test_root + '/mywork/'
 
 # Paths to signal and test_list YAMLs
 test_list_path = root_path + '/test/test_list.yaml'
@@ -28,10 +27,12 @@ cocotb_env = {
     # Add the test modules to PYTHONPATH
     'PYTHONPATH'             : f'${{PYTHONPATH}}:{test_file_dir}',
 
-    # This 
-    'LIBPYTHON_LOC'          : '/snap/gnome-3-38-2004/115/usr/lib/x86_64-linux-gnu/libpython3.8.so',
+    # This path needs to be set individually
+    # Please do a `locate libpython3.10.so`
+    # Choose a path that leads to libpython3.10.so and replace the following with that
+    'LIBPYTHON_LOC'          : '/snap/gnome-42-2204/65/usr/lib/x86_64-linux-gnu/libpython3.10.so',
     
     # These environment variables dictate cocotb logging settings
-    'COCOTB_REDUCED_LOG_FMT' : 'FALSE',
-    'COCOTB_LOG_LEVEL'       : 'DEBUG'
+    'COCOTB_REDUCED_LOG_FMT' : 'TRUE',  # Set this to false to debug any error
+    'COCOTB_LOG_LEVEL'       : 'INFO'   # Set this to 'DEBUG' to get a verbose logging
 }
