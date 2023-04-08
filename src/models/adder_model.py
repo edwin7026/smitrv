47820d9aad6d3bd1458b00840d38dbfb759a48c7
edwin7026 <edwin.joy@incoresemi.com>
2023-04-08 15:28:42 +0530

    Simplified model

--- src/models/adder_model.py
+++ src/models/adder_model.py
@@ -2,34 +2,11 @@
 # module level verification. We consider this to be a reference model
 
 '''
-Clock enabled adder model
+Adder model
 Arguments:
     A (int), B(int) : Inputs to the adder
-    CLK (bool)      ; Clock Pulse
 Returns
     (int)           : Sum of A and B
 '''
-def adder_model(CLK: bool, A: int, B: int) -> int:
-    try:
-        if adder_model.clk == 0 and CLK == 1:   # positive edge
-            adder_model.sum = A + B
-            return adder_model.sum
-        else:
-            return adder_model.sum
-    except AttributeError:          # initial run
-        adder_model.clk = CLK
-        adder_model.sum = 0
-        return adder_model.sum
-
-# Module debugging
-if __name__ == '__main__':
-
-    # Model testing
-
-    print(adder_model(0, 1, 4))
-    print(adder_model(1, 1, 4))
-    print(adder_model(0, 3, 4))
-    print(adder_model(1, 3, 4))
-    print(adder_model(1, 3, 4))
-    print(adder_model(0, 4, 4))
-    print(adder_model(1, 4, 4))
+def adder_model(A: int, B: int) -> int:
+    return A + B
